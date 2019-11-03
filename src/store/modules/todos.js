52d892aa7c09b95f92343856774c73b8fbd2b580
TheBachtiarz <bachtiarptibinlppnri2417@gmail.com>
2019-11-03 14:21:03 +0700

    filter limit todos view finish

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -18,6 +18,12 @@ const actions = {
     async deleteTodo({ commit }, id) {
         await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
         commit('removeTodo', id);
+    },
+    async filterTodos({ commit }, event) {
+        // get selected value
+        const limit = parseInt(event.target.options[event.target.options.selectedIndex].innerText);
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+        commit('setTodos', response.data);
     }
 };
 const mutations = {
